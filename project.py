e125e8b3de5c5e22d9e96cb808afb73c5bfe76f0
Sanjai A <127008722+sanjaizerickson@users.noreply.github.com>
2024-04-25 17:16:26 +0530

    Update project.py

--- project.py
+++ project.py
@@ -57,8 +57,11 @@ def main():
             area = float(area)  # Convert area to float
             prediction = model.predict([[area, bedrooms, bathrooms, stories, mainroad, guestroom, basement, airconditioning, furnishing_status, parking, 0]])[0]
 
-            # Format prediction in lakhs and thousands format manually
-            formatted_prediction = f'₹ {prediction:,.0f}'
+            # Format prediction in Indian format
+            prediction_in_lakhs = prediction / 100000
+            lakhs = int(prediction_in_lakhs)
+            thousands = int(prediction) % 100000
+            formatted_prediction = f'₹ {lakhs:02d},{thousands:03d}'
 
             # Display formatted prediction with bold text and a different background color
             st.success(f'**Predicted Price: {formatted_prediction}**')
